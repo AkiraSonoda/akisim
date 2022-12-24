9c87aa9980ac66f92d1c064172f018dd4dfdaa7e
AkiraSonoda <akira.sonoda.1@gmail.com>
2022-12-24 11:44:20 +0700

    [ARRIBA] 6deeb5d BSActorAvatarMove.cs - cleanup

--- OpenSim/Region/PhysicsModules/BulletS/BSActorAvatarMove.cs
+++ OpenSim/Region/PhysicsModules/BulletS/BSActorAvatarMove.cs
@@ -27,13 +27,9 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using OpenSim.Framework;
 using OpenSim.Region.PhysicsModules.SharedBase;
-
 using OMV = OpenMetaverse;
+// AKIDO: clean
 
 namespace OpenSim.Region.PhysicsModule.BulletS
 {
