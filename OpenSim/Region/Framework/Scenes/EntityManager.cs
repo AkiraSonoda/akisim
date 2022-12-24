5cfab8e5256088025183f9a4b622e4cec167f175
AkiraSonoda <akira.sonoda.1@gmail.com>
2022-12-24 11:01:21 +0700

    [ARRIBA] 6deeb5d EntityManager.cs - cleanup

--- OpenSim/Region/Framework/Scenes/EntityManager.cs
+++ OpenSim/Region/Framework/Scenes/EntityManager.cs
@@ -26,12 +26,10 @@
  */
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Reflection;
-using log4net;
 using OpenMetaverse;
 using OpenSim.Framework;
+// AKIDO: clean
 
 namespace OpenSim.Region.Framework.Scenes
 {
